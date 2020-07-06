139d827bc909c4da3af743cc2772dca72e5b82f9
Wawtuh <98korjor@gmail.com>
2020-07-06 23:22:20 +0900

    Create ClassCtor&NamedArg.dart

---
+++ dart_tutorials/ClassCtor&NamedArg.dart
@@ -0,0 +1,23 @@
+class Person {
+  String name;
+  int age;
+  
+  Person({this.name, this.age = 30});
+}
+
+double addNumbers(double num1, double num2){
+  //print(num1+num2);
+  return num1 + num2;
+}
+
+void main() {
+  var p1 = Person(name: 'Max',age: 30);
+  var p2 = Person(age: 31, name: 'Manu');
+  print(p1.name);
+  print(p2.name);
+  var firstResult = addNumbers(1, 2.6);
+  firstResult = addNumbers(1, 1);
+  // ...
+  print(firstResult + 1);
+  print('Hello!');
+}
