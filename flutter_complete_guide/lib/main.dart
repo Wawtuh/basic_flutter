3b1979f5f80dd549fd6c84d164007ab1ada82470
Wawtuh <98korjor@gmail.com>
2020-07-07 14:01:48 +0900

    Update main.dart

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
-void main() {
-  runApp(MyApp());
-}
+//void main() {
+//  runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hello!'),);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my defalt text!'),
+      ),
+    );
   }
 }
