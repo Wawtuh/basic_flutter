a6c8add7a1cdc61b91afa02b7542ec603b5c5511
Wawtuh <98korjor@gmail.com>
2020-07-07 14:26:51 +0900

    Update main.dart

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -6,6 +6,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Anwer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
     var questoins = [
@@ -22,15 +26,18 @@ class MyApp extends StatelessWidget {
             Text('The question!'),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed:
+                  answerQuestion, // 포인터를 전달, answerQuestion() -> 결과값(void)를 전달
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: null,
+              onPressed: () => print('Answer 2 chosen'),
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: null,
+              onPressed: () {
+                print('Answer 3 chosen');
+              },
             ),
           ],
         ),
