62e198189846dc7c65ff6942c0930c33f44d03fa
Wawtuh <98korjor@gmail.com>
2020-07-07 21:13:19 +0900

    Update ClassCtor&NamedArg.dart

--- dart_tutorials/ClassCtor&NamedArg.dart
+++ dart_tutorials/ClassCtor&NamedArg.dart
@@ -3,6 +3,10 @@ class Person {
   int age;
   
   Person({this.name, this.age = 30});
+  
+  Person.veryOld(this.name){
+    age = 60;
+  }
 }
 
 double addNumbers(double num1, double num2){
@@ -13,6 +17,9 @@ double addNumbers(double num1, double num2){
 void main() {
   var p1 = Person(name: 'Max',age: 30);
   var p2 = Person(age: 31, name: 'Manu');
+  var p3 = Person.veryOld('Max');
+  print(p3.name);
+  print(p3.age);
   print(p1.name);
   print(p2.name);
   var firstResult = addNumbers(1, 2.6);
