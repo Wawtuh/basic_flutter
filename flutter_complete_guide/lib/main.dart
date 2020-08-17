6e58ec013d750e47002685b3a4434d19e154417f
Wawtuh <98korjor@gmail.com>
2020-08-17 18:45:54 +0900

    Update main.dart

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final questoins = const [
+  final questions = const [
     {
       'questionText': 'What\'s your favorite color?',
       'answers': ['Blank', 'Red', 'Greand', 'White'],
@@ -38,8 +38,10 @@ class _MyAppState extends State<MyApp> {
       _questionIndex++;
     });
     print(_questionIndex);
-        if (_questionIndex < questoins.length) {
+    if (_questionIndex < questions.length) {
       print('We have more questions!');
+    } else {
+      print('NO more questions!');
     }
   }
 
@@ -50,20 +52,22 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questoins[_questionIndex]['questionText'],
-            ),
-            ...(questoins[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              // .map(value){return ???} => 리스트의 각각의 원소를 value로 받고 함수안에서 return 값을 Iterable(list, map 등등의 조상?)로 다시 저장
-              return Answer(_answerQuestion, answer);
-            }).toList() // 그래서 toList없어도 작동함
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(
+                    questions[_questionIndex]['questionText'],
+                  ),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    // .map(value){return ???} => 리스트의 각각의 원소를 value로 받고 함수안에서 return 값을 Iterable(list, map 등등의 조상?)로 다시 저장
+                    return Answer(_answerQuestion, answer);
+                  }).toList() // 그래서 toList없어도 작동함
 
-            /// ...은 리스트 안에 리스트가 들어가게 생겼는데 이 안의 리스트의 원소를 쪼개서 넣어서 [A, [B, C, D]] => [A, B, C, D] 이렇게 만들어줌
-          ],
-        ),
+                  /// ...은 리스트 안에 리스트가 들어가게 생겼는데 이 안의 리스트의 원소를 쪼개서 넣어서 [A, [B, C, D]] => [A, B, C, D] 이렇게 만들어줌
+                ],
+              )
+            : Center(child: Text('You did it!')),
       ),
     );
   }
