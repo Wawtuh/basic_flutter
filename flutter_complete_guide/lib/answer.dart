9ca92fe3e6af017a10e9e61967c95ea6bb2ff36f
Wawtuh <98korjor@gmail.com>
2020-08-17 18:40:06 +0900

    update

--- flutter_complete_guide/lib/answer.dart
+++ flutter_complete_guide/lib/answer.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
-  final Function selectHandler;
+  final Function selectHandler; //final: runtime constant <-> const: compile time constant
   final String answerText;
-
   Answer(this.selectHandler, this.answerText);
 
   @override
@@ -12,6 +11,7 @@ class Answer extends StatelessWidget {
       width: double.infinity,
       child: RaisedButton(
         color: Colors.blue,
+        textColor: Colors.white,
         child: Text(answerText),
         onPressed: selectHandler,
       ),
--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -17,6 +17,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questoins = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Blank', 'Red', 'Greand', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'What\'s your favorite Fruit?',
+      'answers': ['Apple', 'Peach', 'Melon', 'grape'],
+    },
+  ];
   int _questionIndex = 0;
 
   void _answerQuestion() {
@@ -24,24 +38,13 @@ class _MyAppState extends State<MyApp> {
       _questionIndex++;
     });
     print(_questionIndex);
+        if (_questionIndex < questoins.length) {
+      print('We have more questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questoins = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Blank', 'Red', 'Greand', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
-      },
-      {
-        'questionText': 'What\'s your favorite Fruit?',
-        'answers': ['Apple', 'Peach', 'Melon', 'grape'],
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -53,9 +56,10 @@ class _MyAppState extends State<MyApp> {
               questoins[_questionIndex]['questionText'],
             ),
             ...(questoins[_questionIndex]['answers'] as List<String>)
-                .map((answer) { // .map(value){return ???} => 리스트의 각각의 원소를 value로 받고 함수안에서 return 값을 Iterable(list, map 등등의 조상?)로 다시 저장
+                .map((answer) {
+              // .map(value){return ???} => 리스트의 각각의 원소를 value로 받고 함수안에서 return 값을 Iterable(list, map 등등의 조상?)로 다시 저장
               return Answer(_answerQuestion, answer);
-            }).toList()// 그래서 toList없어도 작동함
+            }).toList() // 그래서 toList없어도 작동함
 
             /// ...은 리스트 안에 리스트가 들어가게 생겼는데 이 안의 리스트의 원소를 쪼개서 넣어서 [A, [B, C, D]] => [A, B, C, D] 이렇게 만들어줌
           ],
