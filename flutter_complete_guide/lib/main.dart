7e4b31d62d8c8db52a4270507ee387177bab28d3
Wawtuh <98korjor@gmail.com>
2020-08-18 20:59:22 +0900

    Fin

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -51,9 +51,16 @@ class _MyAppState extends State<MyApp> {
   int _questionIndex = 0;
   var _totalScore = 0;
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
-    _totalScore +=score;
-    
+    _totalScore += score;
+
     setState(() {
       _questionIndex++;
     });
@@ -78,7 +85,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
